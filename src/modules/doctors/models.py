d65b3285ecb0e39579935a3aabafd54d44bbc6b0
Ruslan <v209861@gmail.com>
2026-08-13 16:55:33 +0300

    feat: create specialties module and approve alembic

--- src/modules/doctors/models.py
+++ src/modules/doctors/models.py
@@ -10,16 +10,17 @@ class Doctor(Base, Timestamp):
     user_id: Mapped[int] = mapped_column(
         ForeignKey("users.id", ondelete="cascade"), unique=True
     )
-    speciality: Mapped[str]
-    educational: Mapped[str]
+    specialty_id: Mapped[int]
+    education: Mapped[str]
     experience_years: Mapped[int]
     bio: Mapped[str]
 
     __table_args__ = (
         Index(
-            "ix_doctors_speciality_exp", "speciality", "educational", "experience_years"
+            "ix_doctors_speciality_exp", "specialty_id", "education", "experience_years"
         ),
     )
 
     user: Mapped["User"] = relationship(back_populates="doctor")
     suggestions: Mapped[list["Suggestion"]] = relationship(back_populates="doctor")
+    specialties: Mapped[list["Specialty"]] = relationship(back_populates="doctor", secondary="doctor_specialties")
---
+++ src/modules/specialities/models.py
@@ -0,0 +1,29 @@
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from src.core.database import Base, Timestamp
+
+
+class Specialty(Base, Timestamp):
+    __tablename__ = "specialties"
+
+    name: Mapped[str]
+    description: Mapped[str]
+
+    doctors: Mapped[list["Doctor"]] = relationship(
+        secondary="doctor_specialties",
+        back_populates="specialties"
+    )
+
+
+class DoctorSpecialty(Base, Timestamp):
+    __tablename__ = "doctor_specialties"
+
+    id = None
+    doctor_id: Mapped[int] = mapped_column(
+        ForeignKey("doctors.id", ondelete="cascade"), primary_key=True
+    )
+    speciality_id: Mapped[int] = mapped_column(
+        ForeignKey("specialties.id", ondelete="cascade"), primary_key=True
+    )
+
