3e092bf5acad15bcd05a4d0e3a8c8f8efdac492c
Ruslan <v209861@gmail.com>
2026-08-16 16:36:39 +0300

    refactor: create annotations in specialty services

--- src/modules/specialties/service.py
+++ src/modules/specialties/service.py
@@ -6,23 +6,10 @@ from src.modules.specialties.models import Specialty
 from src.modules.specialties.schemas import SpecialtyCreate, SpecialtyUpdate
 
 
-async def get_specialties(
-    db: AsyncSession,
-):
-    query = select(Specialty)
-    result = await db.execute(query)
-    specialties = result.scalars().all()
-    return specialties
-
-
-async def get_specialty_by_id(specialty_id: int, db: AsyncSession):
-    query = select(Specialty).filter(Specialty.id == specialty_id)
-    result = await db.execute(query)
-    specialty = result.scalar_one_or_none()
-    return specialty
-
-
-async def create_specialty(new_specialty: SpecialtyCreate, db: AsyncSession):
+# CREATE
+async def create_specialty(
+    new_specialty: SpecialtyCreate, db: AsyncSession
+) -> Specialty | None:
     try:
         query = (
             insert(Specialty)
@@ -31,23 +18,33 @@ async def create_specialty(new_specialty: SpecialtyCreate, db: AsyncSession):
         )
         result = await db.execute(query)
         created_specialty = result.scalar_one_or_none()
-        return created_specialty
         await db.commit()
+        return created_specialty
     except IntegrityError:
         return None
 
 
-async def delete_specialty(specialty_id: int, db: AsyncSession):
-    query = delete(Specialty).where(Specialty.id == specialty_id).returning(Specialty)
+# READ
+async def get_specialties(
+    db: AsyncSession,
+) -> list[Specialty]:
+    query = select(Specialty)
     result = await db.execute(query)
-    deleted_speciality = result.scalar_one_or_none()
-    await db.commit()
-    return deleted_speciality
+    specialties = list(result.scalars().all())
+    return specialties
 
 
+async def get_specialty_by_id(specialty_id: int, db: AsyncSession) -> Specialty | None:
+    query = select(Specialty).filter(Specialty.id == specialty_id)
+    result = await db.execute(query)
+    specialty = result.scalar_one_or_none()
+    return specialty
+
+
+# UPDATE
 async def update_specialty(
     specialty_id: int, specialty_data: SpecialtyUpdate, db: AsyncSession
-):
+) -> Specialty | None:
     update_data = specialty_data.model_dump(exclude_unset=True)
 
     if not update_data:
@@ -64,3 +61,12 @@ async def update_specialty(
     updated_specialty = result.scalar_one_or_none()
     await db.commit()
     return updated_specialty
+
+
+# DELETE
+async def delete_specialty(specialty_id: int, db: AsyncSession) -> Specialty | None:
+    query = delete(Specialty).where(Specialty.id == specialty_id).returning(Specialty)
+    result = await db.execute(query)
+    deleted_speciality = result.scalar_one_or_none()
+    await db.commit()
+    return deleted_speciality
