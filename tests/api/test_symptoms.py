b86aa6b2e988f992e3853d3b695bb8a81e4b2f3b
Ruslan <v209861@gmail.com>
2026-09-10 15:01:47 +0300

    test: move the logic for creating the symptom to the fixture

--- tests/api/test_symptoms.py
+++ tests/api/test_symptoms.py
@@ -20,6 +20,13 @@ payload = {
 }
 
 
+@pytest.fixture
+async def created_symptom(ac, fake_get_admin_user):
+    response = await ac.post("/symptoms/", json=payload)
+    assert response.status_code == 201
+    return response.json()
+
+
 class TestCreateSymptom:
     async def test_create_symptom(
         self, ac, get_test_session, fake_get_admin_user
@@ -71,16 +78,8 @@ class TestCreateSymptom:
 
 
 class TestReadSymptom:
-    async def test_get_symptoms(self, ac, get_test_session):
-        symptom = Symptom(
-            name="Cough",
-            description="A cough is a reflex action that helps clear the throat and airways of mucus, irritants, or foreign particles.",
-        )
-        get_test_session.add(symptom)
-        await get_test_session.commit()
-
+    async def test_get_symptoms(self, ac, created_symptom, get_test_session):
         response = await ac.get("/symptoms/")
-
         assert response.status_code == 200
         data = response.json()
         assert isinstance(data, list)
@@ -98,35 +97,27 @@ class TestReadSymptom:
         cached_symptoms = await fake_get_redis.getc(cache_key)
         assert cached_symptoms is not None
 
-    async def test_get_symptom_by_id(self, ac, fake_get_admin_user):
-        response_create = await ac.post("/symptoms/", json=payload)
-        assert response_create.status_code == 201
-
-        created_symptom = SymptomRead.model_validate_json(response_create.text)
-        response_get = await ac.get(f"/symptoms/{created_symptom.id}")
-
+    async def test_get_symptom_by_id(self, ac,  created_symptom):
+        response_get = await ac.get(f"/symptoms/{created_symptom["id"]}")
         assert response_get.status_code == 200        
 
         fetched_symptom = SymptomRead.model_validate_json(response_get.text)
-        assert fetched_symptom.id == created_symptom.id
+        assert fetched_symptom.id == created_symptom["id"]
 
     async def test_get_symptom_by_id_not_found_error(self, ac):
         response_get = await ac.get("/symptoms/9999")
         assert response_get.status_code == 404
 
     async def test_get_symptom_by_id_cache(
-        self, ac, fake_get_redis, fake_get_admin_user
+        self, ac, fake_get_redis, created_symptom
     ):
-        response_create = await ac.post("/symptoms/", json=payload)
-        assert response_create.status_code == 201
-        created_symptom = SymptomRead.model_validate_json(response_create.text)        
-
+       
         cache_key = fake_get_redis.build_key("symptoms", "items", "all")
 
         cached_symptoms = await fake_get_redis.getc(cache_key)
         assert cached_symptoms is None
 
-        response_get = await ac.get(f"/symptoms/{created_symptom.id}")
+        response_get = await ac.get(f"/symptoms/{created_symptom["id"]}")
         assert response_get.status_code == 200
 
         cached_symptoms = await fake_get_redis.getc(cache_key)
@@ -134,11 +125,7 @@ class TestReadSymptom:
 
 
 class TestUpadteSymptom:
-    async def test_update_symptom(self, ac, get_test_session, fake_get_admin_user):
-        response_create = await ac.post("/symptoms/", json=payload)
-        assert response_create.status_code == 201
-        created_symptom = response_create.json()
-
+    async def test_update_symptom(self, ac, get_test_session, created_symptom):        
         update_payload = {
             "name": "Updated name",
             "description": "Updated description",
@@ -161,11 +148,7 @@ class TestUpadteSymptom:
         assert db_symptom.name == update_payload["name"]
         assert db_symptom.description == update_payload["description"]
 
-    async def test_update_symptom_cache(self, ac, fake_get_redis, fake_get_admin_user):
-        response_create = await ac.post("/symptoms/", json=payload)
-        assert response_create.status_code == 201
-        created_symptom = response_create.json()
-        
+    async def test_update_symptom_cache(self, ac, fake_get_redis, created_symptom):        
         update_payload = {
             "name": "Updated name",
             "description": "Updated description",
@@ -185,34 +168,18 @@ class TestUpadteSymptom:
         assert cached_symptom is None
                 
     async def test_update_symptom_not_found_error(self, ac, fake_get_admin_user):
-        response_create = await ac.post("/symptoms/", json=payload)
-        assert response_create.status_code == 201        
-        
-        update_payload = {
-            "name": "Updated name",
-            "description": "Updated description",
-        }
-        
         response = await ac.patch(
-            "/symptoms/9999", json=update_payload
+            "/symptoms/9999", json=payload
         )
         assert response.status_code == 404
 
             
 class TestDeleteSymptom:
-    async def test_delete_symptom(self, ac, fake_get_admin_user):
-        response_create = await ac.post("/symptoms/", json=payload)
-        assert response_create.status_code == 201        
-        created_symptom = response_create.json()
-        
+    async def test_delete_symptom(self, ac, created_symptom):        
         response = await ac.delete(f"/symptoms/{created_symptom["id"]}")
         assert response.status_code == 204
 
-    async def test_update_symptom_cache(self, ac, fake_get_redis, fake_get_admin_user):
-        response_create = await ac.post("/symptoms/", json=payload)
-        assert response_create.status_code == 201        
-        created_symptom = response_create.json()        
-
+    async def test_update_symptom_cache(self, ac, fake_get_redis, created_symptom):
         cache_key = fake_get_redis.build_key("symptoms", "items", "all")
 
         await fake_get_redis.setc(cache_key, payload, CacheTTL.STATIC)
@@ -226,10 +193,7 @@ class TestDeleteSymptom:
         assert cached_symptom is None
         
                 
-    async def test_update_symptom_not_found_error(self, ac, fake_get_admin_user):
-        response_create = await ac.post("/symptoms/", json=payload)
-        assert response_create.status_code == 201        
-
+    async def test_update_symptom_not_found_error(self, ac, fake_get_admin_user):      
         response = await ac.delete("/symptoms/9999")
         assert response.status_code == 404
 
